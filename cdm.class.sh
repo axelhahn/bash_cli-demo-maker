1ce818afd904bc2f17887027c8a5bbd7d4583493
Axel Hahn <ax2002@gmx.net>
2024-08-14 12:15:06 +0200

    apply fake path in prompt; rename cdm.waitOnEnd to cdm.end; root user is red

--- cdm.class.sh
+++ cdm.class.sh
@@ -8,7 +8,7 @@
 # ----------------------------------------------------------------------
 # license: GNU GPL 3.0
 # source: <https://github.com/axelhahn/bash_cli-demo-maker>
-# docs:   coming soon <https://www.axel-hahn.de/docs/bash_cli-demo-maker/>
+# docs:   <https://www.axel-hahn.de/docs/bash_cli-demo-maker/>
 # author: Axel Hahn * www.axel-hahn.de
 # ----------------------------------------------------------------------
 # 2024-05-nn  v0.1  initial version
@@ -17,18 +17,21 @@
 # 2024-05-30  v0.4  add wait time before executing a command
 # 2024-05-30  v0.5  detect timeout and write new line; fix in cdm.script
 # 2024-07-24  v0.6  apply waittime at the end too; show 
+# 2024-08-14  v0.7  apply fake path in prompt; rename cdm.waitOnEnd to cdm.end; root user is red
 # ======================================================================
 
-CDM_VERSION='0.6'
-CDM_ABOUT='Axels cli demo maker'
+CDM_VERSION='0.7'
 
-# a username in the prompt
+# data for prompt: a username, a dummy hostname and a path 
+# (empty string = always show current dir)
 CDM_USER='user'
 CDM_HOST='tux-client'
+CDM_PATH=''
 
 CDM_WAITTIME=''
 
 COLOR_PRESET_prompt_user='green'
+COLOR_PRESET_prompt_userroot='red'
 COLOR_PRESET_prompt_host='lightgray'
 COLOR_PRESET_prompt_path='cyan'
 COLOR_PRESET_prompt_char='lightgray'
@@ -51,7 +54,13 @@ function cdm.sethost(){
     CDM_HOST="$1"
 }
 
-# set a user name for the prompt
+# Set a fixed path for the prompt to hide the current location
+# param  string  new path name; default is "" (=dynamicly written current dir)
+function cdm.setpath(){
+    CDM_PATH="$1"
+}   
+
+# Set a user name for the prompt
 # param  string  new user name; default is "user"
 function cdm.setuser(){
     CDM_USER="$1"
@@ -102,13 +111,22 @@ function cdm.timer(){
 # ----------------------------------------------------------------------
 
 # Render a fake shell prompt
-# see cdm.setHost to set a different host
-# see cdm.setUser to set a different user
+# see cdm.sethost to set a different host
+# see cdm.setuser to set a different user
+# see cdm.setpath to set a fixed path to hide real path
 function cdm.prompt(){
     local _path; 
-    _path=$( pwd )
-    _path=$( echo "$_path" | sed "s#^/home/[a-z]*/#~/#g" )
-    color.print $COLOR_PRESET_prompt_user "$CDM_USER"
+    local _colUser=$COLOR_PRESET_prompt_user
+    test "$CDM_USER" = "root" && _colUser=$COLOR_PRESET_prompt_userroot
+
+    _path="$CDM_PATH"
+    if [ -z "$_path" ]; 
+    then 
+        _path=$( pwd )
+        _path=$( echo "$_path" | sed "s#^/home/[a-z]*/#~/#g" )
+    fi
+
+    color.print $_colUser "$CDM_USER"
     color.print $COLOR_PRESET_prompt_host "@$CDM_HOST "
     color.print $COLOR_PRESET_prompt_path "$_path"
     color.print $COLOR_PRESET_prompt_char "> "
@@ -159,20 +177,27 @@ function cdm.shell(){
 }
 
 # Randomly delayed typing of a given text
+# Dealy time is random between 0.0 and 0.250 sec
 # param string text
 function cdm.typer(){
     local _txt="$1"
+    local _sleep; typeset -i _sleep
+    local _sleep2
     for ((i=0; i<=${#_txt}; i++)); do
         printf '%s' "${_txt:$i:1}"
-        _sleep=0.$(( (RANDOM % 1000)/300 ))
-        sleep "$_sleep"
+        _sleep=$(( RANDOM % 250 ))
+        _sleep2=$_sleep
+        test $_sleep -lt 10  && _sleep2=0$_sleep2
+        test $_sleep -lt 100 && _sleep2=0$_sleep2
+        _sleep2=0.$_sleep2
+        sleep "$_sleep2"
     done 
 }
 
 
 # show a waiting text after a demo
 # param  string  optional: custom text; default: "End of this demo. Press Return ..."
-function cdm.waitOnEnd(){
+function cdm.end(){
     local _text="${1:- >>> End of this demo. Press Return ... }"
     local dummy
     local readparam=
@@ -184,9 +209,10 @@ function cdm.waitOnEnd(){
     sleep 0.2; echo
     sleep 0.1; echo
     sleep 0.05; echo
-    color.echo darkgray "________________________________________________________________________"
-    test -z "$1" && color.echo darkgray " Created with <https://github.com/axelhahn/bash_cli-demo-maker>"; sleep 3; echo; echo; echo; echo
-    
+    # color.echo darkgray "______________________________________________________________________________"
+    # test -z "$1" && color.echo darkgray "Created with CLI DEMO MAKER  <https://github.com/axelhahn/bash_cli-demo-maker>"; sleep 3; echo; echo; echo; echo
+
+    color.reset
     echo -n "$_text"
     read -r -s $readparam dummy
     echo
@@ -231,18 +257,20 @@ function cdm.script() {
             fi
         fi
 
-        _firstword=$( echo "${_prefix}${array[@]}" | cut -f 1 -d " " | grep "cdm\." )
+        _firstword=$( echo "${_prefix}${array[*]}" | cut -f 1 -d " " | grep "cdm\." )
         echo -n "$_firstword" >> "$_exec"
-        echo "${_prefix}${array[@]}${_suffix}" | sed 's#^cdm\.[a-z][a-z]*##gi' >> "$_exec"
+        echo "${_prefix}${array[*]}${_suffix}" | sed 's#^cdm\.[a-z][a-z]*##gi' >> "$_exec"
         
     done
     if [ -n "$_debug" ]; then
-        echo "DEUBUG: generated code:"
+        echo "DEBUG: generated code:"
         echo
-        cat "$_exec" | sed "s#^#    #g"
+        sed "s#^#    #g" "$_exec"
         echo
-        echo "DEUBUG: execute it:"
+        echo -n "DEBUG: wait time: "
+        test -z "$CDM_WAITTIME" && echo "interactive - wait for RETURN" || echo "wait for RETURN or max. $CDM_WAITTIME sec"
     fi
+    # shellcheck source=/dev/null
     . "$_exec"
     rm -f "$_exec"
 }
